e951de5af3e9842dd2e56f779ef7fe22619df47b
matheussatiro <matheussatiro00@gmail.com>
2026-02-06 22:15:01 -0300

    Add files via upload

---
+++ exer2.java
@@ -0,0 +1,15 @@
+package lista;
+
+public class exer2 {
+public static void main(String[] args) {
+    int num;
+    num = 18;
+
+    if(num >= 18){
+        System.out.println("Você é maior de idade, bem vindo ao Café show Jonas");
+    }else{
+        System.out.println("Jonas, tenha paciencia, quem sabe ano que vem");
+    }
+}
+    
+}
---
+++ exer3.java
@@ -0,0 +1,19 @@
+package lista;
+
+public class exer3 {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        num1 = 20;
+        num2 = 20;
+
+        if(num1 > num2){
+            System.out.println("Numero 1 é maior que numero 2");
+        }else if(num1 < num2){
+            System.out.println("Numero 2 é maior que o número 1");
+        }else
+            System.out.println("são Números Iguais");
+        System.out.println("Sempre executar");
+        }
+    
+}
